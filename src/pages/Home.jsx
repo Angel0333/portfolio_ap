9291a8f831df3e1dd04f8ff00f81822a92265062
Ángel Pastrana <Angel Pastranaangelpastrana0333@gmail.com>
2025-06-04 18:58:58 -0300

    Primer commit agregando lo que seria la estructura de la pagina y algunos componentes como Navbar, Footer, Home; Aclaro que aun no tiene contenido

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+
+function Home() {
+
+  return (
+    <>
+      <Navbar />
+      <div>
+        <h1>Hola soy el Home</h1>
+      </div>
+
+      <Footer />
+    </>
+  )
+}
+
+export default Home
