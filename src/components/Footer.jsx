3bc4cfa6afad82afbfcb44acf7b55c3cd75be726
Ángel Pastrana <Angel Pastranaangelpastrana0333@gmail.com>
2025-07-27 22:41:44 -0300

    agrego transciciones en el logo y los botones de redes sociales

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,25 +14,25 @@ function Footer() {
           {/* centro */}
           <div className="mb-2 md:mb-0">
             <a href="#">
-              <img src={logo} alt="Logo" className="h-12 w-12 rounded-full object-cover " />
+              <img src={logo} alt="Logo" className="transition-all duration-300 transform hover:scale-125 h-12 w-12 rounded-full object-cover " />
             </a>
           </div>
 
           {/* derecha */}
           <div className="flex space-x-4 text-xl">
-            <a href="https://www.linkedin.com/in/angel-pastrana-b7856223b/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400">
+            <a href="https://www.linkedin.com/in/angel-pastrana-b7856223b/" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 transform hover:scale-125 hover:text-blue-400 ">
               <FaLinkedin />
             </a>
 
-            <a href="https://github.com/Angel0333" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+            <a href="https://github.com/Angel0333" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 transform hover:scale-125 hover:text-gray-400" >
               <FaGithub />
             </a>
 
-            <a href="https://wa.me/+5493812060571" target="_blank" rel="noopener noreferrer" className="hover:text-green-400">
+            <a href="https://wa.me/+5493812060571" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 transform hover:scale-125 hover:text-green-400">
               <FaWhatsapp />
             </a>
 
-            <a href="https://www.instagram.com/angelpastrana_15?igsh=bTV5bWY5cTBsbnh1" target="_blank" rel="noopener noreferrer" className="hover:text-pink-400">
+            <a href="https://www.instagram.com/angelpastrana_15?igsh=bTV5bWY5cTBsbnh1" target="_blank" rel="noopener noreferrer" className="transition-all duration-300 transform hover:scale-125 hover:text-pink-400">
               <FaInstagram />
             </a>
 
