1c9d842213c55293e4913bb0dcd14639c8d2309f
Ángel Pastrana <Angel Pastranaangelpastrana0333@gmail.com>
2025-07-30 22:36:35 -0300

    agrego el componente service y tambien utilizo react router dom para navegar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
 import Home from './pages/Home'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { HOME, SERVICE } from './components/routers/router'
+import Service from './pages/Service'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Home/>
+    <Routes>
+      <Route path={HOME} element={<Home />} />
+      <Route path={SERVICE} element= {<Service/>}/>
+
+    </Routes>
+      
     </>
   )
 }
